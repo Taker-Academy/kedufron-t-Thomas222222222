78d703359dfb8b585688d7dc0c8e94c0013b0768
Thomas <thomas.roche-rebelle@epitech.eu>
2024-03-03 17:20:04 +0100

    [ADD] Function to add item in localstorage of cart

--- scripts/panier.js
+++ scripts/panier.js
@@ -1,3 +1,43 @@
+function createLocalStorage(_id) {
+    var firstObject = [{id: _id, quantity: 1}];
+
+    localStorage.setItem('cart', JSON.stringify(firstObject));
+}
+
+function productExist(id, storedProducts) {
+    if (storedProducts === null) {
+        return false;
+    }
+    for (var i = 0; i < storedProducts.length; i++) {
+        if (storedProducts[i].id === id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function addProduct(_id) {
+    var storedProducts = JSON.parse(localStorage.getItem('cart'));
+    var new_product;
+
+    if (productExist(_id, storedProducts) === true) {
+        for (var i = 0; i < storedProducts.length; i++) {
+            if (storedProducts[i].id === _id) {
+                storedProducts[i].quantity = storedProducts[i].quantity + 1;
+            }
+        }
+    } else {
+        new_product = {id: _id, quantity: 1};
+        storedProducts.push(new_product);
+    }
+    localStorage.setItem('cart', JSON.stringify(storedProducts));
+}
+
 function addProductToCart(id) {
-    console.log("Add item");
+    // localStorage.clear();
+    if (localStorage.getItem('cart') !== null) {
+        addProduct(id);
+    } else {
+        createLocalStorage(id);
+    }
 }
